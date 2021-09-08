85181973c9346055bbca7a9f0c67733b20589d1e
Cybre3 <71160954+Cybre3@users.noreply.github.com>
2021-09-08 16:52:15 -0400

    added timetravel

---
+++ src/history.js
@@ -0,0 +1,83 @@
+
+function history(){
+    return [
+    // Before first move
+        {
+            squares: [
+                null, null, null,
+                null, null, null,
+                null, null, null,
+            ]
+        },
+        // After first move
+        {
+            squares: [
+                null, null, null,
+                null, null, null,
+                null, null, null,
+            ]
+        },
+        // After second move
+        {
+            squares: [
+                null, null, null,
+                null, null, null,
+                null, null, null,
+            ]
+        },
+        // After third move
+        {
+            squares: [
+                null, null, null,
+                null, null, null,
+                null, null, null,
+            ]
+        },
+        {
+            squares: [
+                null, null, null,
+                null, null, null,
+                null, null, null,
+            ]
+        },
+        {
+            squares: [
+                null, null, null,
+                null, null, null,
+                null, null, null,
+            ]
+        },
+        {
+            squares: [
+                null, null, null,
+                null, null, null,
+                null, null, null,
+            ]
+        },
+        {
+            squares: [
+                null, null, null,
+                null, null, null,
+                null, null, null,
+            ]
+        },
+        {
+            squares: [
+                null, null, null,
+                null, null, null,
+                null, null, null,
+            ]
+        },
+        {
+            squares: [
+                null, null, null,
+                null, null, null,
+                null, null, null,
+            ]
+        },
+    ];
+}
+
+module.exports = {
+    history,
+}
