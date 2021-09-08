607b896f290b546caa57b3f7fe61b811eeea4b0c
Cybre3 <71160954+Cybre3@users.noreply.github.com>
2021-09-08 15:46:14 -0400

    React tutorial_tic-tac-toe_Completed

---
+++ src/Components/Square.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "../Game.css";
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+// class Square extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       value: null,
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <button
+//         className="square"
+//         onClick={() => {
+//           this.props.onClick();
+//         }}
+//       >
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+
+export default Square;
